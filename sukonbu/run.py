bbcfe0b210d2e3b64e15665b6782f77d19db69d9
ousttrue <ousttrue@gmail.com>
2019-11-28 18:00:20 +0900

    traverse

--- sukonbu/run.py
+++ sukonbu/run.py
@@ -1,21 +1,32 @@
 import sys
 import pathlib
 import json
-from typing import NamedTuple, Any, List, Dict
+import io
+from typing import NamedTuple, Any, List, Dict, Optional, TextIO
 
 
 class JsonSchema(NamedTuple):
-    title: str
+    title: str = ''
     type: str = 'object'
     description: str = ''
     gltf_detailedDescription: str = ''
-    # allOf: List[Any] = None
+    default: Any = None
+    gltf_webgl: Any = None
+    gltf_uriType: Any = None
     properties: Dict[str, 'JsonSchema'] = {}
-    items: Any = None
+    #
+    additionalProperties: Any = None
+    minProperties: Any = None
+    items: Optional['JsonSchema'] = None
     uniqueItems: Any = None
     minItems: Any = None
+    maxItems: Any = None
     pattern: str = ''
+    format: str = ''
     minimum: Any = None
+    maximum: Any = None
+    exclusiveMinimum: Any = None
+    multipleOf: Any = None
     #
     dependencies: List[Any] = []
     required: List[Any] = []
@@ -23,27 +34,32 @@ class JsonSchema(NamedTuple):
     @staticmethod
     def from_dict(root: Dict[str, Any]) -> 'JsonSchema':
         def traverse(node: Dict[str, Any]):
+            # pass replace leaf to JsonSchema
             props = node.get('properties')
             if props:
-                for key, prop in props.items():
-                    traverse(prop)
-                    if 'title' not in prop:
-                        prop['title'] = key
                 node['properties'] = {
-                    key: JsonSchema(**prop)
+                    key: traverse(prop)
                     for key, prop in props.items()
                 }
+            items = node.get('items')
+            if items:
+                node['items'] = traverse(items)
+            return JsonSchema(**node)
 
-        traverse(root)
-        return JsonSchema(**root)
+        return traverse(root)
 
-    def __str__(self) -> str:
+    def traverse(self):
+        if self.type == 'array':
+            for x in self.items.traverse():
+                yield x
+        elif self.type == 'object':
+            for k, v in self.properties.items():
+                for x in v.traverse():
+                    yield x
+        yield self
+
+    def __str__(self):
         return f'[{self.title}: {self.type}]'
-
-    def print_tree(self, indent=''):
-        print(f'{indent}{self}')
-        for key, prop in self.properties.items():
-            prop.print_tree(indent + '  ')
 
 
 def preprocess(path: pathlib.Path):
@@ -64,6 +80,9 @@ def preprocess(path: pathlib.Path):
             elif key == 'properties':
                 for k, v in node[key].items():
                     traverse(v, parents + [k])
+            elif key == 'items':
+                items = traverse(node[key], parents + ['[]'])
+                node[key] = items
             elif key == 'allOf':
                 value = node[key]
                 if len(value) == 1:
@@ -83,33 +102,51 @@ def preprocess(path: pathlib.Path):
                     'type',
                     'description',
                     'gltf_detailedDescription',
+                    'gltf_webgl',
+                    'gltf_uriType',
+                    'default',
                     #
                     'additionalProperties',
-                    'items',
+                    'minProperties',
+                    #
                     'uniqueItems',
                     'minItems',
-                    'pattern',
+                    'maxItems',
+                    #
                     'minimum',
+                    'maximum',
+                    'multipleOf',
+                    'exclusiveMinimum',
+                    'pattern',
+                    'format',
+                    #
                     #
                     'required',
                     'dependencies',
             ]:
                 pass
+            elif key in ['anyOf', 'not', 'oneOf']:
+                del node[key]
             elif key == '$ref':
                 ref = preprocess(path.parent / node[key])
-                node[key] = ref
+                node = ref
             else:
                 raise Exception(f'unknown {key}')
+        return node
 
-    traverse(parsed, [])
-    return parsed
+    return traverse(parsed, [])
 
 
 def process(entry_point: pathlib.Path):
     print(entry_point)
     parsed = preprocess(entry_point)
     root = JsonSchema.from_dict(parsed)
-    root.print_tree()
+    # sio = io.StringIO()
+    # print(sio.getvalue())
+
+    for js in root.traverse():
+        if js.type == 'object':
+            print(js)
 
 
 def main():
