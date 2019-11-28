a0c119da4e5ceebd6a131ff54045690bbf1f7ca0
ousttrue <ousttrue@gmail.com>
2019-11-28 20:37:08 +0900

    fix traverse

--- sukonbu/run.py
+++ sukonbu/run.py
@@ -70,87 +70,89 @@ class JsonSchema(NamedTuple):
             a = 0
 
 
-def preprocess(path: pathlib.Path):
-    text = path.read_text()
-    parsed = json.loads(text)
-    if not isinstance(parsed, dict):
-        raise TypeError('not dict')
-    parsed['path'] = path
+def preprocess(parsed: dict, directory: pathlib.Path):
+    if '$schema' in parsed:
+        del parsed['$schema']
 
-    def traverse(node, parents):
-        keys = [key for key in node.keys()]
-        for key in keys:
-            if key == '$schema':
-                del node['$schema']
-            # elif key == '$ref':
-            #     ref = dir / node[key]
-            #     print(ref)
-            #     raise NotImplementedError()
-            elif key == 'properties':
-                for k, v in node[key].items():
-                    traverse(v, parents + [k])
-            elif key == 'items':
-                items = traverse(node[key], parents + ['[]'])
-                node[key] = items
-            elif key == 'allOf':
-                value = node[key]
-                if len(value) == 1:
-                    # inherit $ref
-                    ref = preprocess(path.parent / value[0]['$ref'])
-                    for k, v in ref.items():
-                        # add members
-                        if k in node:
-                            NotImplementedError()
-                        else:
-                            node[k] = v
-                    del node[key]
-                else:
-                    raise NotImplementedError()
-            elif key in [
-                    'path',
-                    'title',
-                    'type',
-                    'description',
-                    'gltf_detailedDescription',
-                    'gltf_webgl',
-                    'gltf_uriType',
-                    'default',
-                    #
-                    'additionalProperties',
-                    'minProperties',
-                    #
-                    'uniqueItems',
-                    'minItems',
-                    'maxItems',
-                    #
-                    'minimum',
-                    'maximum',
-                    'multipleOf',
-                    'exclusiveMinimum',
-                    'pattern',
-                    'format',
-                    #
-                    #
-                    'required',
-                    'dependencies',
-            ]:
-                pass
-            elif key in ['anyOf', 'not', 'oneOf']:
-                del node[key]
-            elif key == '$ref':
-                ref = preprocess(path.parent / node[key])
-                node = ref
-            else:
-                raise Exception(f'unknown {key}')
-        return node
+    if '$ref' in parsed:
+        # replace
+        path = directory / parsed['$ref']
+        text = path.read_text()
+        ref_parsed = json.loads(text)
+        ref = preprocess(ref_parsed, path.parent)
+        for k, v in ref.items():
+            parsed[k] = v
+        del parsed['$ref']
 
-    return traverse(parsed, [])
+    if 'allOf' in parsed:
+        # inherited
+        path = directory / parsed['allOf'][0]['$ref']
+        text = path.read_text()
+        ref_parsed = json.loads(text)
+        ref = preprocess(ref_parsed, path.parent)
+        for k, v in ref.items():
+            if k in parsed:
+                if k == 'properties':
+                    for pk, pv in ref[k].items():
+                        parsed[k][pk] = pv
+                    continue
+                elif k in ['title']:
+                    continue
+            parsed[k] = v
+        del parsed['allOf']
+
+    for key in ['anyOf', 'not', 'oneOf']:
+        if key in parsed:
+            del parsed[key]
+
+    keys = [key for key in parsed.keys()]
+    for key in keys:
+        if key == 'properties':
+            for k, v in parsed[key].items():
+                preprocess(v, directory)
+        elif key == 'items':
+            parsed[key] = preprocess(parsed[key], directory)
+        elif key in [
+                'path',
+                'title',
+                'type',
+                'description',
+                'gltf_detailedDescription',
+                'gltf_webgl',
+                'gltf_uriType',
+                'default',
+                #
+                'additionalProperties',
+                'minProperties',
+                #
+                'uniqueItems',
+                'minItems',
+                'maxItems',
+                #
+                'minimum',
+                'maximum',
+                'multipleOf',
+                'exclusiveMinimum',
+                'pattern',
+                'format',
+                #
+                #
+                'required',
+                'dependencies',
+        ]:
+            pass
+        else:
+            raise Exception(f'unknown {key}')
+
+    return parsed
 
 
 def process(entry_point: pathlib.Path):
     print(entry_point)
-    parsed = preprocess(entry_point)
-    root = JsonSchema.from_dict(parsed)
+    text = entry_point.read_text()
+    parsed = json.loads(text)
+    processed = preprocess(parsed, entry_point.parent)
+    root = JsonSchema.from_dict(processed)
     # sio = io.StringIO()
     # print(sio.getvalue())
 
