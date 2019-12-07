693ac98c70725906cdc64ef74553bf1d1eff9ce1
ousttrue <ousttrue@gmail.com>
2019-12-08 01:06:28 +0900

    add generators/dlang.py

---
+++ sukonbu/cli.py
@@ -0,0 +1,47 @@
+import sys
+import pathlib
+import json
+import io
+from typing import NamedTuple, Any, List, Dict, Optional, TextIO
+from sukonbu.json_schema_parser import JsonSchemaParser
+from sukonbu.generators import python, dlang
+import argparse
+
+
+def main():
+    '''
+    sukonbu.py {json_path} [--lang python] [--dst dir]
+    '''
+    parser = argparse.ArgumentParser(description='sukonbu.')
+    parser.add_argument('json', help='target json file.')
+    parser.add_argument('--lang',
+                        default='python',
+                        choices=['python', 'dlang'],
+                        help='generate language.')
+    parser.add_argument('--dst', help='output directory.')
+    args = parser.parse_args()
+
+    # source json path
+    if not args.json:
+        parser.print_help()
+    path = pathlib.Path(args.json)
+    if not path.exists():
+        raise FileExistsError(path)
+
+    # parse
+    js_parser = JsonSchemaParser()
+    js_parser.process(pathlib.Path(path))
+
+    if args.dst:
+        dst = pathlib.Path(args.dst)
+        if args.lang == 'python':
+            python.generate(js_parser, dst)
+        else:
+            dlang.generate(js_parser, dst)
+
+    else:
+        js_parser.print()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sukonbu/generators/dlang.py
@@ -0,0 +1,7 @@
+import pathlib
+from ..json_schema_parser import JsonSchemaParser
+
+
+def generate(self: JsonSchemaParser, dst: pathlib.Path) -> None:
+    a = 0
+    pass
