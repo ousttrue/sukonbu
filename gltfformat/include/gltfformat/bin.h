8c11cc93318b57153fdf787834ddda9b94da67f3
ousttrue <ousttrue@gmail.com>
2020-03-10 22:28:55 +0900

    add get_bytes(const BufferView &view)

--- gltfformat/include/gltfformat/bin.h
+++ gltfformat/include/gltfformat/bin.h
@@ -73,17 +73,20 @@ public:
         m_buffer.push_back({glbBin, byteLength});
     }
 
+    buffer get_bytes(const BufferView &view) const
+    {
+        auto &buffer = m_gltf.buffers[view.buffer.value()];
+        auto bytes = m_buffer[view.buffer.value()];
+        return bytes
+            .slice(0, buffer.byteLength.value())
+            .slice(view.byteOffset.value_or(0), view.byteLength.value());
+    }
+
     buffer get_bytes(const Accessor &accessor) const
     {
         auto &view = m_gltf.bufferViews[accessor.bufferView.value()];
-        auto &buffer = m_gltf.buffers[view.buffer.value()];
-        auto &bytes = m_buffer[view.buffer.value()];
-
         auto accessor_stride = get_stride(accessor);
-
-        return bytes
-            .slice(0, buffer.byteLength.value())
-            .slice(view.byteOffset.value_or(0), view.byteLength.value())
+        return get_bytes(view)
             .slice(accessor.byteOffset.value_or(0), accessor.count.value() * accessor_stride);
     }
 };
